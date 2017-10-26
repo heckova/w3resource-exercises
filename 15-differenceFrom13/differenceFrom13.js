ff3c4cb5cbb5202e7afcf4ba5a783b92373a75bc
Alta <heckova.l@gmail.com>
2017-10-26 15:56:37 +0100

    ex15-function can compare number 13, positive and negative number

--- 15-differenceFrom13/differenceFrom13.js
+++ 15-differenceFrom13/differenceFrom13.js
@@ -1,12 +1,17 @@
 function compare(num) {
-    if (num > 13) {
+    if (num >= 13) {
         difference = Math.abs(num - 13) * 2;
     }
-    else {
+    else if (num < 13 && num >= 0) {
         difference = Math.abs(num - 13);
+    }
+    else if (num < 0) {
+        difference = Math.abs(num) + 13;
     }
     return difference;
 }
 
-console.log(compare(32));
+console.log(compare(13));
+console.log(compare(15));
 console.log(compare(11));
+console.log(compare(-1));
